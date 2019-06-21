4a0a307da52f939e5d94c877f414f6f39cbd8b30
Grace Colette <ilovegrace09@gmail.com>
2019-06-21 17:32:50 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,20 @@
+def my_collect(students) 
+  while students.length > 0 
+  # students.collect do |student|
+  #   student.split(" ").first
+  # puts students
+  # end
+  students
+  # yield(lang)
+end
+
+# my_collect(students) do |name|
+#     name.split(" ").first
+
+# my_collect(lang) {|lang|
+#   lang.upcase}
+  
+ 
+   
 
 
